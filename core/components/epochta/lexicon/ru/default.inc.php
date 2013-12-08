8d2f170e9b9ac376205eff9be7f5d6c93babc1e4
Clean <admclean@yandex.ru>
2013-12-09 01:49:33 +0400

    code life cycle and generate algoritm

--- core/components/epochta/lexicon/ru/default.inc.php
+++ core/components/epochta/lexicon/ru/default.inc.php
@@ -3,15 +3,5 @@
 include_once 'setting.inc.php';
 
 $_lang['epochta'] = 'ePochta';
-$_lang['epochta_menu_desc'] = 'Пример расширения для разработки.';
-$_lang['epochta_items'] = 'Предметы';
-$_lang['epochta_item_create'] = 'Создать предмет';
-$_lang['epochta_item_err_ae'] = 'Предмет с таким именем уже существует.';
-$_lang['epochta_item_err_nf'] = 'Предмет не найден.';
-$_lang['epochta_item_err_ns'] = 'Предмет не указан.';
-$_lang['epochta_item_err_remove'] = 'Ошибка при удалении Предмета.';
-$_lang['epochta_item_err_save'] = 'Ошибка при сохранении Предмета.';
-$_lang['epochta_item_remove'] = 'Удалить Предмет';
-$_lang['epochta_item_remove_confirm'] = 'Вы уверены, что хотите удалить этот Предмет?';
-$_lang['epochta_item_update'] = 'Изменить Предмет';
-$_lang['epochta_intro_msg'] = 'Управляйте вашими предметами.';
+
+$_lang['ep_sms_timeout_failure'] = 'Должен быть перерыв между отправкой сообщений!';
--- core/components/epochta/processors/web/validate/create.class.php
+++ core/components/epochta/processors/web/validate/create.class.php
@@ -1,73 +1,113 @@
 <?php
-class epValidateNumCreateProcessor extends modObjectCreateProcessor {
+class epValidateNumCreateProcessor extends modObjectCreateProcessor
+{
 
     public $object;
 
-    public $classKey='epValidateNum';
-
-
-
+    public $classKey = 'epValidateNum';
+    public $languageTopics = array('epochta:default');
 
 
     /**
      * @param $number
      * @return string
      */
-    function gen_pass($number)
+    function gen_code($number)
     {
-        //array of passw symbols
-        $arr = array('a','b','c','d','e','f',
-            'g','h','i','j','k','l',
-            'm','n','o','p','r','s',
-            't','u','v','x','y','z',
-            'A','B','C','D','E','F',
-            'G','H','I','J','K','L',
-            'M','N','O','P','R','S',
-            'T','U','V','X','Y','Z',
-            '1','2','3','4','5','6',
-            '7','8','9','0','.',',',
-            '(',')','[',']','!','?',
-            '&','^','%','@','*','$',
-            '<','>','/','|','+','-',
-            '{','}','`','~');
+        //array of code symbols
+        $arr = array('a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'r', 's',
+            't', 'u', 'v', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'R', 'S',
+            'T', 'U', 'V', 'X', 'Y', 'Z',
+            '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', '0', '.', ',',
+            '(', ')', '[', ']', '!', '?',
+            '&', '^', '%', '@', '*', '$',
+            '<', '>', '/', '|', '+', '-',
+            '{', '}', '`', '~');
 
         // generate password
-        $pass = "";
-        for($i = 0; $i < $number; $i++)
-        {
+        $code = "";
+        for ($i = 0; $i < $number; $i++) {
             // get random index
             $index = rand(0, count($arr) - 1);
-            $pass .= $arr[$index];
+            $code .= $arr[$index];
         }
-        return $pass;
+        return $code;
     }
 
     /**
      * {@inheritDoc}
      * @return boolean|string
      */
-    public function initialize() {
-        $pass=$this->gen_pass(6);
+    public function initialize()
+    {
+
 
         $this->setProperties(array(
-            'user_id' =>$this->modx->user->id
-        ,'code' =>$pass
-        ,'validate' => 0
-        ,'ip' => $this->modx->request->getClientIp()
-        ,'createdon' => date('Y-m-d H:i:s')
-        ,'updatedon' =>date('Y-m-d H:i:s')
+            'user_id' => $this->modx->user->id
+        , 'validate' => 0
+        , 'ip' => $this->modx->request->getClientIp()
+        , 'createdon' => date('Y-m-d H:i:s')
+        , 'updatedon' => date('Y-m-d H:i:s')
 
         ));
         return parent::initialize();
     }
 
 
+    public function beforeSet()
+    {
+        $timeout = 15;
+        $codelifetime = 15 * 60 * 24;
+
+        $query = $this->modx->newQuery('epValidateNum');
+        /*get row with select all from max row with max createdon  */
+        $query->select('id as id,createdon as createdon,code as code,validate as validate');
+        $query->limit = 1;
+        $query->sortby('createdon', 'desc');
+        $query->where(array('user_id:=' => $this->getProperty('user_id'),));
 
 
+        if ($query->prepare() && $query->stmt->execute()) {
+            $data = $query->stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        print_r($data);
 
-    public function afterSave() {
+        $datediff = abs(strtotime($data['createdon']) - strtotime($this->getProperty('createdon')));
+
+        //check timeout behind user|sms
+        if ($datediff < $timeout) {
+            echo "fail";
+            return $this->failure($this->modx->lexicon('sms_timeout_failure'));
+        }
+
+
+        //check if we already send code to user, and he doens't validate them
+        if (($data['validate'] == 0) and ($datediff < $codelifetime)) {
+            //if code is not validate and lifecycle is not end -send old gen. code.
+            $this->setProperties(array('code' => $data['code'],));
+
+        } else {
+            //generate new code
+            $code = $this->gen_code(6);
+            $this->setProperties(array('code' => $code,));
+        }
+
+
+        return parent::beforeSave();
+
+    }
+
+
+    public function afterSave()
+    {
         //send sms in code
-        $this->modx->epochta->sendSMS_now($this->object->get('phone'),$this->object->get('code'),0);
+    //    $this->modx->epochta->sendSMS_now($this->object->get('phone'), $this->object->get('code'), 0);
 
         return parent::afterSave();
     }
